aedfcb95e156cf9e7f72ef00c5e9316a5bfe816c
Medyuk, Aleksandr <a.medyuk@a1qa.com>
2019-10-02 15:00:41 +0300

    init

--- src/test/java/TestBoolean.java
+++ src/test/java/TestBoolean.java
@@ -3,8 +3,9 @@ import org.testng.annotations.Test;
 public class TestBoolean {
     @Test
     public void test() {
-        if(Boolean.getBoolean(System.getenv("isRemote"))){
+        if(Boolean.parseBoolean(System.getenv("isRemote"))){
             System.out.println("isRemote");
+            System.out.println(System.getenv("isRemote"));
         } else {
             System.out.println("Not isRemote");
             System.out.println(System.getenv("isRemote"));
